720cde003c94d3473ac9bbf69aaedde6b3d495e7
Ryan DM <ryan.m.de.man@gmail.com>
2022-10-07 17:11:51 +0200

    javascript toegevoegd voor menubar responsive

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    
+});
